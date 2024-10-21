8bad816286c079e31f093f8c8f44606325a7308a
ludwigal <alexandersson_ludwig@yahoo.se>
2024-10-21 11:37:07 +0200

    Co-authored-by: Oskar Meyer <Oskmey@users.noreply.github.com> Co-authored-by: HermanForsberg <HermanForsberg@users.noreply.github.com> Co-authored-by: AronLundh <AronLundh@users.noreply.github.com>

--- FlaskServer/app/__init__.py
+++ FlaskServer/app/__init__.py
@@ -4,7 +4,9 @@ from webuiapi import webuiapi
 from flask import request, jsonify
 from PIL import Image
 import io
-
+import base64
+import requests
+from io import BytesIO
 app = Flask(__name__)
 
 api = webuiapi.WebUIApi()
@@ -16,15 +18,48 @@ def index():
 @app.route("/upload", methods=["POST"])
 def generate_image():
 
-    image = Image.open(request.files['image'].stream)
+    image = Image.open(request.files['image'])
+    image.save("input_image.png")
     prompt = request.form.get('prompt')
     sampler = request.form.get('sampler')
     steps = int(request.form.get('steps'))
     cfg_scale = float(request.form.get('cfg_scale'))
+    denoising_strength = float(request.form.get('denoising_strength'))
 
-    result = api.img2img(images=[image], prompt=prompt, sampler_name=sampler, steps=steps, cfg_scale=cfg_scale)
+    
+    #print(cfg_scale)
+
+    result = api.img2img(images=[image], prompt=prompt, sampler_name=sampler, steps=steps, cfg_scale=cfg_scale, denoising_strength=denoising_strength, width=1000, height=1000)
+
+    print(result)
+
+    result.images[0].save("output_image.png")
+
+    # url = "http://127.0.0.1:7860"
+
+    # payload = {
+    #     "prompt": "puppy dog",
+    #     "steps": 5
+    # }
+
+    # response = requests.post(url=f'{url}/sdapi/v1/img2img', json=payload)
+    # r = response.json()
 
 
+    
+    
+    # image_data = base64.b64decode(result.images[0])
+
+    # with open('output_image.png', 'wb') as file:
+    #     file.write(image_data)
 
 
-    return jsonify(result)
+    # def decode_and_save_base64(base64_str, save_path):
+    # with open(save_path, "wb") as file:
+    #     file.write(base64.b64decode(base64_str))
+
+    return result
+
+# def encode_file_to_base64(path):
+#     with open(path, 'rb') as file:
+#         return base64.b64encode(file.read()).decode('utf-8')
--- FlaskServer/app/svelte/index/flask.js
+++ FlaskServer/app/svelte/index/flask.js
@@ -1,11 +1,12 @@
-export function uploadImage(image, promt="cute squirrel", sampler = "Euler a", steps = 20, cfg_scale=7.0) {
+export function uploadImage(image, prompt="cozy, autumn, low lighting", sampler = "Euler a", steps = 15, cfg_scale=7, denoising_strength=0.7) {
     const formData = new FormData();
 
     formData.append('image', image);
-    formData.append('promt', promt);
+    formData.append('prompt', prompt);
     formData.append('sampler', sampler);
     formData.append('steps', steps);
     formData.append('cfg_scale', cfg_scale);
+    formData.append('denoising_strength', denoising_strength);
 
     fetch('/upload', {
         method: 'POST',
